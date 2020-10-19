8e4a567efca74e5e5a144dd764c9cc928979da32
Yash Gupta <32073855+itsyash2122@users.noreply.github.com>
2020-10-19 15:12:00 +0530

    Revert "Updated counting_sort.py"

--- sorts/counting_sort.py
+++ sorts/counting_sort.py
@@ -1,17 +1,16 @@
 """
-This is a pure Python implementation of counting sort algorithm.
-For doctests run the following command:
+This is pure Python implementation of counting sort algorithm
+For doctests run following command:
 python -m doctest -v counting_sort.py
 or
 python3 -m doctest -v counting_sort.py
-
-For the manual testing run:
+For manual testing run:
 python counting_sort.py
 """
 
 
 def counting_sort(collection):
-    """A pure implementation of counting sort algorithm in Python
+    """Pure implementation of counting sort algorithm in Python
     :param collection: some mutable ordered collection with heterogeneous
     comparable items inside
     :return: the same collection ordered by ascending
@@ -23,32 +22,32 @@ def counting_sort(collection):
     >>> counting_sort([-2, -5, -45])
     [-45, -5, -2]
     """
-    # If the collection is empty, it returns empty
-    if collection == []: 
+    # if the collection is empty, returns empty
+    if collection == []:
         return []
 
-    # Get some information about the collection
+    # get some information about the collection
     coll_len = len(collection)
     coll_max = max(collection)
     coll_min = min(collection)
 
-    # Creating the counting array.
+    # create the counting array
     counting_arr_length = coll_max + 1 - coll_min
     counting_arr = [0] * counting_arr_length
 
-    # Count the times how much a number appears in the collection.
+    # count how much a number appears in the collection
     for number in collection:
         counting_arr[number - coll_min] += 1
 
-    # Sum each position with it's predecessors. Now, counting_arr[i] tells 
-    # us how many elements less than equals to i are in the collection.
+    # sum each position with it's predecessors. now, counting_arr[i] tells
+    # us how many elements <= i has in the collection
     for i in range(1, counting_arr_length):
         counting_arr[i] = counting_arr[i] + counting_arr[i - 1]
 
-    # Create an output collection
+    # create the output collection
     ordered = [0] * coll_len
 
-    # Place the elements in the output, keeping the original order (stable
+    # place the elements in the output, respecting the original order (stable
     # sort) from end to begin, updating counting_arr
     for i in reversed(range(0, coll_len)):
         ordered[counting_arr[collection[i] - coll_min] - 1] = collection[i]
@@ -59,15 +58,15 @@ def counting_sort(collection):
 
 def counting_sort_string(string):
     """
-    >>> counting_sort_string("thisisastring")
-    'aghiiinrssstt'
+    >>> counting_sort_string("thisisthestring")
+    'eghhiiinrsssttt'
     """
     return "".join([chr(i) for i in counting_sort([ord(c) for c in string])])
 
 
 if __name__ == "__main__":
-    # Testing of string sort
-    assert "aghiiinrssstt" == counting_sort_string("thisisastring")
+    # Test string sort
+    assert "eghhiiinrsssttt" == counting_sort_string("thisisthestring")
 
     user_input = input("Enter numbers separated by a comma:\n").strip()
     unsorted = [int(item) for item in user_input.split(",")]
